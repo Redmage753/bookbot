a52df7ba968e809ff4af4c6473c1b7269617fbb5
redmage753 <firehazard159@gmail.com>
2025-06-16 23:45:01 -0500

    broke out stats.py separately

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(path):
+    with open(path) as f:
+        content = f.read()
+    f.close()
+    return content
+
+def main():
+    num_words= get_num_words(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document.")
+    #print(get_book_text("books/frankenstein.txt"))
+    
+main()
+    
---
+++ stats.py
@@ -0,0 +1,7 @@
+def get_num_words(book_text):
+    split_text=book_text.split()
+    count=0
+    for i in split_text:
+        count+=1
+    return count
+

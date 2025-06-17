77dd6d16b31646ddd0d5abab5775e70f3d001612
redmage753 <firehazard159@gmail.com>
2025-06-17 00:22:33 -0500

    Added char_counter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import get_num_words
+from stats import char_counter
+from stats import sort_on
 
 def get_book_text(path):
     with open(path) as f:
@@ -10,6 +12,9 @@ def main():
     num_words= get_num_words(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document.")
     #print(get_book_text("books/frankenstein.txt"))
-    
+    char_count=char_counter(get_book_text("books/frankenstein.txt"))
+    char_count.sort(reverse=True, key=sort_on)    
+    print(char_count)
+
 main()
     
--- stats.py
+++ stats.py
@@ -5,3 +5,23 @@ def get_num_words(book_text):
         count+=1
     return count
 
+def char_counter(text):
+    lower_text=text.lower()
+    char_list={}
+    char_set=set()
+    count=0
+    for letter in lower_text:
+        #print(f"{letter} to add")
+        char_set.add(letter)
+        if letter in char_list:
+            char_list[letter]+=1
+        else:
+            char_list[letter]=1
+    return char_list
+
+def sort_num(dict):
+    sorted_dict={}
+    for i in 
+    return sorted_dict
+
+
